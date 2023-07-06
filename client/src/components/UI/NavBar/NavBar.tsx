8a361e38c39ed9efb766e9df70feaacff6f5fe70
Vi Truong <vitruong@Vis-MacBook-Pro.hsd1.tx.comcast.net>
2023-07-05 21:07:04 -0500

    added contextual menu

--- client/src/components/UI/NavBar/NavBar.tsx
+++ client/src/components/UI/NavBar/NavBar.tsx
@@ -1,9 +1,30 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import React, { useState, useRef } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  Divider,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { AccountCircle } from "@mui/icons-material";
+import { AccountCircle, Logout, Settings } from "@mui/icons-material";
+import ListItemIcon from "@mui/material/ListItemIcon";
 
 const NavBar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isContextualMenuOpen = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="fixed">
       <Toolbar>
@@ -24,15 +45,73 @@ const NavBar = () => {
         >
           Careness
         </Typography>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="menu-appbar"
-          aria-haspopup="true"
-          color="inherit"
+        <Avatar sx={{ width: 32, height: 32 }} onClick={handleClick}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls={isContextualMenuOpen ? "account-menu" : undefined}
+            aria-expanded={isContextualMenuOpen ? "true" : undefined}
+            aria-haspopup="true"
+            color="inherit"
+          >
+            V
+          </IconButton>
+        </Avatar>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={isContextualMenuOpen}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&:before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
         >
-          <AccountCircle />
-        </IconButton>
+          <MenuItem onClick={handleClose}>
+            <Avatar /> Profile
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Avatar /> My account
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Settings fontSize="small" />
+            </ListItemIcon>
+            Settings
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
